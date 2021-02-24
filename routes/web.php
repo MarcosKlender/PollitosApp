39b7df6175b89177eee373baffe07119e37c6fff
deibypi <deibypi12@hotmail.com>
2021-02-24 11:38:47 -0500

    Rutas para reportes
    Se añade rutas "reportes"

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,13 @@ Route::put('/profile', 'ProfileController@update')->name('profile.update');
 Route::resource('admin', AdminController::class);
 Route::resource('proveedores', ProveedoresController::class);
 
+
+
 Route::get('/ajax-autocomplete-search', 'PesoBrutoController@selectSearch')->name('selectSearch');
+Route::get('/pesobruto/peso_bascula','PesoBrutoController@pesobascula')->name('pesobascula');
+
+//Route::post('/reportes/ajaxshowdetallepesobruto','ReportesController@index');
+
 
 Route::get('/pesobruto/lotes_anulados', 'PesoBrutoController@lotes_anulados')->name('pesobruto.lotes_anulados');
 Route::post('/pesobruto/anular_lote', 'PesoBrutoController@anular_lote')->name('pesobruto.anular_lote');
@@ -35,7 +41,12 @@ Route::get('/pesobruto/registros_anulados', 'PesoBrutoController@registros_anula
 Route::post('/pesobruto/anular_registro', 'PesoBrutoController@anular_registro')->name('pesobruto.anular_registro');
 Route::post('/pesobruto/registrar_gavetas', 'PesoBrutoController@registrar_gavetas')->name('pesobruto.registrar_gavetas');
 Route::post('/pesobruto/liquidar_lote', 'PesoBrutoController@liquidar_lote')->name('pesobruto.liquidar_lote');
-Route::resource('pesobruto', PesoBrutoController::class);
 
+//Route::post('/reportes/show_detalle', 'ReportesController@show_detalle')->name('reportes.show_detalle');
+
+
+Route::resource('pesobruto', PesoBrutoController::class);
 Route::resource('pesoneto', PesoNetoController::class);
 Route::resource('entregas', EntregasController::class);
+Route::resource('reportes', ReportesController::class);
+
