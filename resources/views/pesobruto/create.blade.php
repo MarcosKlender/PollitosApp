22ace423896a85fbf278dab53aff9d730cbbb51e
deibypi <deibypi12@hotmail.com>
2021-02-24 12:42:39 -0500

    Ajax para filtrar proveedores

--- resources/views/pesobruto/create.blade.php
+++ resources/views/pesobruto/create.blade.php
@@ -111,7 +111,7 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
-    {{-- <script type="text/javascript">
+    <script type="text/javascript">
         $('#proveedor').select2({
             placeholder: 'Seleccione el proveedor',
             ajax: {
@@ -138,7 +138,7 @@
             document.getElementById("ruc").value = p.value;
         }
 
-    </script> --}}
+    </script> 
 
 
 @endsection
