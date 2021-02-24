b2e0c29d33731d60a6324fbdf0b27c0ee03a8953
deibypi <deibypi12@hotmail.com>
2021-02-24 11:26:03 -0500

    Criterios de busqueda para Reportes de peso bruto
    Se añaden funciones con Scope para criterios de búsqueda de peso bruto, estás se llaman desde controlador ReportesController.php en la función show

--- app/Lotes.php
+++ app/Lotes.php
@@ -19,4 +19,55 @@ class Lotes extends Model
     {
         return $this->hasMany(Registros::class);
     }
+
+    public function scopeTipo($query, $tipo ){
+    	if($tipo)
+    		return $query->where('tipo', 'ilike', "%$tipo%");
+    }
+
+    public function scopeProveedor($query, $proveedor ){
+    	if($proveedor)
+    		return $query->where('proveedor', 'ilike', "%$proveedor%");
+    }
+
+    public function scopeProcedencia($query, $procedencia ){
+    	if($procedencia)
+    		return $query->where('procedencia', 'ilike', "%$procedencia%");
+    }
+
+    public function scopePlaca($query, $placa ){
+    	if($placa)
+    		return $query->where('placa', 'ilike', "%$placa%");
+    }
+
+    public function scopeConductor($query, $conductor ){
+    	if($conductor)
+    		return $query->where('conductor', 'ilike', "%$conductor%");
+    }
+
+     public function scopeUsuario($query, $usuario ){
+    	if($usuario)
+    		return $query->where('usuario', 'ilike', "%$usuario%");
+    }
+
+     public function scopeAnulado($query, $anulado ){
+    	if($anulado)
+    		return $query->where('anulado', $anulado);
+    }
+
+    public function scopeLiquidado($query, $liquidado ){
+    	if($liquidado)
+    		return $query->where('liquidado', $liquidado);
+    }
+
+     public function scopeFechaini($query, $fechaini ){
+    	if($fechaini)
+    		return $query->where('created_at', $fechaini);
+    }
+
+    public function scopeFechafin($query, $fechafin ){
+    	if($fechafin)
+    		return $query->where('created_at', $fechafin);
+    }
+
 }
