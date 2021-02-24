82655a54eb86e768ab7598b1fa90e1862a08d61a
deibypi <deibypi12@hotmail.com>
2021-02-24 11:26:58 -0500

    Modelo de Proveedores
    Se añaden campos de tabla para proveedores que se graban en la base de datos

--- app/Proveedores.php
+++ app/Proveedores.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Proveedores extends Model
 {
     use HasFactory;
+
+    protected $connection = 'pgsql';
+
+    protected $table = 'proveedores';
+
+    protected $fillable = [
+                'tipo','ruc_ci','nombres','razon_social','direccion','telefono','movil','email',
+                'provincia','ciudad','parroquia','created_at','updated_at'
+    ];
+
+    protected $primarykey = 'ruc_ci';
+
+    public $timestamps = false;
 }
