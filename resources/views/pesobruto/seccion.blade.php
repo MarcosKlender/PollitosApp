5396afcb6de050ed6d26296a2ad36f15bf8e692b
deibypi <deibypi12@hotmail.com>
2021-11-23 09:27:49 -0500

    Update disabled
    Se añade "disabled" en el input peso_bruto

--- resources/views/pesobruto/seccion.blade.php
+++ resources/views/pesobruto/seccion.blade.php
@@ -1,1 +1,1 @@
-<input type="text" class="form-control" id="peso_bruto" name="peso_bruto" required value=<?php echo $res ?> >
+<input type="text" class="form-control" id="peso_bruto" name="peso_bruto" disabled required value=<?php echo $res ?> >
