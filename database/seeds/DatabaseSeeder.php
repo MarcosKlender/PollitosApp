8ff58771a49e5af901314ff6f523d70df0b3180d
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2021-12-13 20:33:46 -0500

    Update proveedores
    Se añade en el Seeder proveedores

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -50,12 +50,12 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([
-            'rol_id' => 3,
+            'rol_id' => 1,
             'username' => 'dmorocho',
             'name' => 'Deiby',
             'last_name' => 'Morocho',
-            'email' => 'deiby@gmail.com',
-            'password' => Hash::make('Ecuador2020'),
+            'email' => 'deibypi12@gmail.com',
+            'password' => Hash::make('Dmorocho2020'),
             'created_at' => Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon::now()->toDateTimeString(),
             'active' => 1,
@@ -65,15 +65,94 @@ class DatabaseSeeder extends Seeder
         DB::table('proveedores')->truncate();
 
         DB::table('proveedores')->insert([
-            'pro_ruc' => '0000000000001',
-            'pro_nombre' => 'PRONACA SD',
-            'pro_nombre_comercial' => 'PRONACA SANTO DOMINGO',
+            'pro_ruc' => '1790319857001',
+            'pro_nombre' => 'PRONACA',
+            'pro_nombre_comercial' => 'PRONACA',
             'pro_telefonos' => Str::random(10),
             'pro_email' => Str::random(10).'@gmail.com',
             'pro_direccion' => Str::random(10),
             'created_at' => Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '1712005063001',
+            'pro_nombre' => 'CEVALLOS INTRIAGO SUSANA PATRICIA',
+            'pro_nombre_comercial' => 'IPROCA',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '1715947626001',
+            'pro_nombre' => 'PEREZ ATIENCIA RAMIRO DARIO',
+            'pro_nombre_comercial' => 'SERPEC',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '1791306961001',
+            'pro_nombre' => 'AVICOLA VITALOA',
+            'pro_nombre_comercial' => 'AVITALSA',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '0993211281001',
+            'pro_nombre' => 'AVES Y COMESTIBLES',
+            'pro_nombre_comercial' => 'AVES Y COMESTIBLES',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '1791294505001',
+            'pro_nombre' => 'REPROIMAV',
+            'pro_nombre_comercial' => 'REPROIMAV',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '2390017793001',
+            'pro_nombre' => 'CORPROAVIC',
+            'pro_nombre_comercial' => 'CORPROAVIC',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('proveedores')->insert([
+            'pro_ruc' => '1718632589001',
+            'pro_nombre' => 'GAROFALO JIMENEZ DORIS AIDA',
+            'pro_nombre_comercial' => 'GAROFALO JIMENEZ DORIS AIDA',
+            'pro_telefonos' => Str::random(10),
+            'pro_email' => Str::random(10).'@gmail.com',
+            'pro_direccion' => Str::random(10),
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+
 
         // TABLA LOTES
         DB::table('lotes')->truncate();
